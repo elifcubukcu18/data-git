cd3a4cc8b0f4e67f52420ea8d49bf956603b6aee
Elif Çubukcu <elifcubukcu@192.168.1.101>
2026-02-09 15:27:00 +0300

    Fix style issues, should get a 'Good Style' now :pray:

--- today.py
+++ today.py
@@ -1,12 +1,9 @@
-'''
-This is the module docstring
-'''
-
+"""A module computing buddy pair names for the day"""
 
 pass  # YOUR CODE HERE
 def my_name_is():
-
+    """Return my name."""
     return "Elif"
 def my_buddy_is():
-
+    """Return my buddy's GitHub username."""
     return "elifcubukcu18"
